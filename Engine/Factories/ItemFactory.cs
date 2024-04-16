1118e74ca5ee557676c8dd2dff59450117c104eb
EricCanaveral <156819110+EricCanaveral@users.noreply.github.com>
2024-04-16 23:25:23 +0800

    Update ItemFactory.cs

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -13,16 +13,16 @@ namespace Engine.Factories
         static ItemFactory()
         {
             _standardGameItems = new List<GameItem>();
-            _standardGameItems.Add(new GameItem(64, "Bear Jelly Coins", 1, "BearJellyCoins.png"));
-            _standardGameItems.Add(new GameItem(65, "Star Jelly", 10,"StarJelly.png"));
-            _standardGameItems.Add(new GameItem(66, "Cookie Powder", 10, "CookiePowder.png"));
-            _standardGameItems.Add(new GameItem(67, "Sugar Crystals", 10, "SugarCrystals.png"));
-            _standardGameItems.Add(new GameItem(68, "Insignia Cookie Treasures", 10, "InsigniaCookieTreasure.png"));
-            _standardGameItems.Add(new GameItem(69, "Crystal Jam", 10, "CrystalJam.png"));
-            _standardGameItems.Add(new GameItem(70, "Golden Croissant", 10, "GoldenCroissant.png"));
-            _standardGameItems.Add(new GameItem(71, "The Jelly of Memories", 10, "TheJellyOfMemories.png"));
-            _standardGameItems.Add(new Weapon(72, "Cookie Axe", 10, "CookieAxe.png", 2, 7));
-            _standardGameItems.Add(new Weapon(73, "Cookie Gun", 10, "CookieGun.png", 2, 7));
+            _standardGameItems.Add(new GameItem(64, "CrossBow", 1, "crossbow.jpg"));
+            _standardGameItems.Add(new GameItem(65, "Elixir Of Papaya", 10,"elixir of papaya.jpg"));
+            _standardGameItems.Add(new GameItem(66, "LeafWings", 10, "leafwings.jpg"));
+            _standardGameItems.Add(new GameItem(67, "PapayaBag", 10, "papaya bag.jpg"));
+            _standardGameItems.Add(new GameItem(68, "Papaya Seeds", 10, "papaya seeds.jpg"));
+            _standardGameItems.Add(new GameItem(69, "Papaya Sword", 10, "papaya sword.jpg"));
+            _standardGameItems.Add(new GameItem(70, "Papaya Tree", 10, "papaya tree.jpg"));
+            _standardGameItems.Add(new GameItem(71, "Papaya Wings", 10, "papaya wing.jpg"));
+            _standardGameItems.Add(new Weapon(72, "Seed Shield", 10, "seed shield.jpg", 2, 7));
+            _standardGameItems.Add(new Weapon(73, "SeedLings", 10, "seedlings.jpg", 2, 7));
 
         }
         public static GameItem CreateGameItem(int itemTypeID)
