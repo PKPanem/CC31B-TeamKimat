99ab813d48fb87f3df1271c70bce8f4880e17a13
EricCanaveral <156819110+EricCanaveral@users.noreply.github.com>
2024-04-17 12:10:42 +0800

    Update MonsterFactory.cs

--- Engine/Factories/MonsterFactory.cs
+++ Engine/Factories/MonsterFactory.cs
@@ -16,47 +16,47 @@ namespace Engine.Factories
             {
                 case 1:
                     Monster caveTrolls =
-                        new Monster("Cave Troll", "cavetroll.png", 15, 15, 10, 10);
+                        new Monster("Papayagos the  Enigmatic", "Papayagos the  Enigmatic.png    ", 15, 15, 10, 10);
                     caveTrolls.CurrentWeapon = ItemFactory.CreateGameItem(1502);
                     return caveTrolls;
                 case 2:
                     Monster lavaGolem =
-                        new Monster("Lava Golem", "lavagolem.png", 40, 40, 20, 30);
+                        new Monster("Papayakor The Malevolent", "Papayakor The Malevolent.png", 40, 40, 20, 30);
                     lavaGolem.CurrentWeapon = ItemFactory.CreateGameItem(1503);
                     AddLootItem(lavaGolem, 2502, 100);
                     return lavaGolem;
 
                 case 3:
                     Monster forestImp =
-                        new Monster("Forest Imp", "forestimp.png", 10, 10, 5, 5);
+                        new Monster("Papayathorn the Shadowed", "Papayathorn the Shadowed.png", 10, 10, 5, 5);
                     forestImp.CurrentWeapon = ItemFactory.CreateGameItem(1501);
                     AddLootItem(forestImp, 2501, 100);
                     return forestImp;
 
                 case 4:
                     Monster waterDragon =
-                        new Monster("Water Elemental Dragon", "waterdragon.png", 70, 70, 50, 100);
+                        new Monster("Silent Sentinel", "Silent Sentinel.jpg", 70, 70, 50, 100);
                     waterDragon.CurrentWeapon = ItemFactory.CreateGameItem(1504);
                     AddLootItem(waterDragon, 2503, 100);
                     return waterDragon;
 
                 case 5:
                     Monster fireDragon =
-                        new Monster("Fire Elemental Dragon", "firedragon.png", 120, 120, 100, 500);
+                        new Monster("Captain Papayazmo,Lord of the Rotten Fruits", "captain papayazmo,Lord of the Rotten Fruits.jpg", 120, 120, 100, 500);
                     fireDragon.CurrentWeapon = ItemFactory.CreateGameItem(1505);
                     AddLootItem(fireDragon, 2504, 100);
                     return fireDragon;
 
                 case 6:
                     Monster ghost =
-                        new Monster("Ghost", "ghost.png", 20, 20, 15, 20);
+                        new Monster("Golden Guardian", "golden guardian.jpg", 20, 20, 15, 20);
                     ghost.CurrentWeapon = ItemFactory.CreateGameItem(1506);
                     AddLootItem(ghost, 2505, 100);
                     return ghost;
 
                 case 7:
                     Monster ancientGuardian =
-                        new Monster("Ancient Guardian", "ancientguardian.png", 150, 150, 10, 5);
+                        new Monster("Lady Papayatriz,Mistress of decay", "lady papayatriz,mistress of decay.jpg", 150, 150, 10, 5);
                     ancientGuardian.CurrentWeapon = ItemFactory.CreateGameItem(1507);
                     AddLootItem(ancientGuardian, 2506, 100);
                     return ancientGuardian;
