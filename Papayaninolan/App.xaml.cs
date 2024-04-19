3719f5627f1dceecaa2ed1e5b10e254489e266e6
PKPanem <156826047+PKPanem@users.noreply.github.com>
2024-04-19 19:17:15 +0800

    Update App.xaml.cs

--- Papayaninolan/App.xaml.cs
+++ Papayaninolan/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CC31B_TeamKimat
+namespace CC31B-TeamKimat
 {
     /// <summary>
     /// Interaction logic for App.xaml
